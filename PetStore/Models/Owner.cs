1f3a189b39cbe8cf4cbca055faf7877ccc24014c
Zhaosheng Liu <jonathan888999@163.com>
2020-05-29 18:46:25 -0400

    Add project files.

---
+++ PetStore/Models/Owner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetStore.Models
+{
+    public class Owner
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "First name must not be more than 20 char")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Last name must not be more than 20 char")]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName => this.FirstName + " " + this.LastName;
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Street must not be more than 20 char")]
+        public string Street { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "City must not be more than 20 char")]
+        public string City { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Name must not be more than 20 char")]
+        public string Occupation { get; set; }
+
+        public ICollection<Pet> Enrollments { get; set; }
+    }
+}
---
+++ PetStore/Models/Pet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetStore.Models
+{
+    public enum Breed
+    {
+        Dog, Cat, Fish, Hamster, GuineaPig
+    }
+
+    public class Pet
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessage = "Name must not be more than 10 char")]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime DOB { get; set; }
+
+        public Breed Breed { get; set; }
+
+        [Required]
+        public int OwnerId { get; set; }
+
+        public Owner Owner { get; set; }
+    }
+}
